b31da8a63570fbbf5071d6e2498a01a4e35a2904
Luis Muhele <luismuhele@luismuhele.luismuhele>
2026-03-01 08:33:57 +0100

    refactor: reorganizar os meus imports

--- src/app.py
+++ src/app.py
@@ -2,11 +2,8 @@ from fastapi import FastAPI
 import uvicorn
 
 from routes import users_routes, product_routes
-
-"""
-from db.db import Base, engine
-from models import Order, Product, User
-"""
+# from db.db import Base, engine
+# from models import order_model, product_model, user_model
 
 app = FastAPI(
     title="Kadituxi Store API",
@@ -21,3 +18,4 @@ app.include_router(router=product_routes.products_router, prefix="/products")
 if __name__ == "__main__":
     # Base.metadata.create_all(engine)
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+
